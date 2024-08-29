064b7d01970efecd0e0bc27a43cec158fe39a933
Alexsander Gutierrez Gonçalves <38378566+Alex5ander@users.noreply.github.com>
2024-08-28 23:45:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if("serviceWorker" in navigator){
     window.addEventListener("load", function(e){
-        navigator.serviceWorker.register("sw.js").
+        navigator.serviceWorker.register("/calculadoraclassica/sw.js", { scope: "/calculadoraclassica" }).
         then((registration) => {
             console.log(registration);
         }, (err) => {
